828e4f483888dbf87f943fb257c48bb0193ae59b
Abhi <abhimanyu18n@gmail.com>
2023-10-03 08:49:16 +0100

    Fixed consumer DAG

--- dags/consumer.py
+++ dags/consumer.py
@@ -6,7 +6,7 @@ from datetime import datetime
 my_file = Dataset("/tmp/my_file.txt")
 
 with DAG(
-    dag_id = "consumer"
+    dag_id = "consumer",
     schedule = [my_file],
     start_date = datetime(2022, 1, 1),
     catchup = False
